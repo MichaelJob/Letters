1aeb2d087c38d494fc9515da341feab3f8f6bdae
Michael Job <mj@michaeljob.ch>
2026-03-26 14:18:12 +0100

    fix for safe space at bottom

--- composeApp/src/commonMain/kotlin/ch/michaeljob/letters/ui/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/ch/michaeljob/letters/ui/MainScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -85,7 +86,9 @@ fun MainScreen(
             )
         },
         bottomBar = {
-            HistoryBar(history = history)
+            NavigationBar {
+                HistoryBar(history = history)
+            }
         }
     ) { innerPadding ->
         Column(
